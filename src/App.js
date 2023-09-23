a609746d7572757b9a68a25b69a24b98d0dcb30f
Maijuddin Mallick <124431155+Maijuddin@users.noreply.github.com>
2023-09-23 21:34:52 +0530

    Book-store

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from './Components/SearchBar';
+import "./style.css"
+function App() {
+  return (
+    <div className="App">
+      <logo />
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
